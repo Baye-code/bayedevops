1b011b3ce9248b8e33fd0b9fac4dfa0ed044c2b7
Baye Sabarane <bayesabarane@gmail.com>
2022-05-16 00:36:45 +0000

    Initial commit for project startup

---
+++ src/main/java/com/bayedevops/BayedevopsApplication.java
@@ -0,0 +1,13 @@
+package com.bayedevops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BayedevopsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BayedevopsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bayedevops/BayedevopsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bayedevops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BayedevopsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
