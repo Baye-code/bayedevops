bfb90afacb039c0577860324493ecf4604a3ca1d
Baye Sabarane <bayesabarane@gmail.com>
2022-05-23 17:47:03 +0000

    Added logging setup with logback

--- src/test/java/com/bayedevops/BayedevopsApplicationTests.java
+++ src/test/java/com/bayedevops/BayedevopsApplicationTests.java
@@ -3,22 +3,18 @@ package com.bayedevops;
 import com.bayedevops.web.i18n.I18NService;
 import org.junit.Assert;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
+//import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 //import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
+//import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+//import org.springframework.test.context.web.WebAppConfiguration;
 
 @SpringBootTest
 //@RunWith(SpringJUnit4ClassRunner.class)
 ////@SpringApplication(classes = BayedevopsApplication.class)
 //@WebAppConfiguration
 class BayedevopsApplicationTests {
-
-//	@Test
-//	void contextLoads() {
-//	}
 
 	@Autowired
 	private I18NService i18NService;
